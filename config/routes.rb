b4122fb9c2a2e2061b528b8e7a7c831dc29f39d8
jdesilvio <john.desilvio@yahoo.com>
2015-08-16 13:16:26 -0400

    install Devise and set up user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pages/home'
-
   get 'pages/info'
 
   # The priority is based upon order of creation: first created -> highest priority.
